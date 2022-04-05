e45c1d9044589192429ef5502f30c3e6abd9a775
dajo04002 <92377225+dajo04002@users.noreply.github.com>
2022-04-05 17:49:21 +0200

    Add files via upload

---
+++ MyMonoGameDesktopApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyMonoGameDesktopApp;
+
+namespace  MyMonogameGameDesktopApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using(Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+}
